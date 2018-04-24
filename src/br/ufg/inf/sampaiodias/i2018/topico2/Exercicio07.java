2958e95d02627499c5a1ca4584a2c82046bde9c4
sampaiodias <lucassampaiodias@gmail.com>
2018-04-23 21:20:26 -0300

    Arquivo base Tópico 02 Exercício 07

---
+++ src/br/ufg/inf/sampaiodias/i2018/topico2/Exercicio07.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.sampaiodias.i2018.topico2;
+
+import java.net.HttpURLConnection;
+
+/**
+ * Classe que resolve o Exercício 07 do Tópico 02 da disciplina de Integração
+ * de Aplicações (Semestre 2018-1) de Engenharia de Software.
+ * 
+ * @author Lucas Sampaio Dias
+ */
+public class Exercicio07{
+    public static void main(String[] args) {
+    }
+}

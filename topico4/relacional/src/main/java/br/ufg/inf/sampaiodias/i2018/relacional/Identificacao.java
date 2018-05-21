0e999621320b025f10134f2b684282ff3e247bbd
sampaiodias <lucassampaiodias@gmail.com>
2018-05-21 19:47:26 -0300

    Projeto relacional para Tópico 04

---
+++ topico4/relacional/src/main/java/br/ufg/inf/sampaiodias/i2018/relacional/Identificacao.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018.
+ * Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package br.ufg.inf.sampaiodias.i2018.relacional;
+
+/**
+ * Classe que resolve o Exercício 05 do Tópico 03 da disciplina de Integração
+ * de Aplicações (Semestre 2018-1) de Engenharia de Software.
+ * 
+ * @author Lucas Sampaio Dias
+ */
+public class Identificacao {
+    
+}
